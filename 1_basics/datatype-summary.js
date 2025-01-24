54a4694f7861c27122f5ac84b9c825f4c374024d
Sandesh <163713139+Mark-085@users.noreply.github.com>
2025-01-24 05:00:34 +0000

    datatype summary done

---
+++ 1_basics/datatype-summary.js
@@ -0,0 +1,38 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+ const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+const heros=["ironman","captain america","thor"]
+let myObj={
+    name:"sandesh",
+    age:22
+}
+const myFunction=function(){
+   console.log("hellooo");
+}
+
+console.log(typeof heros)
+console.log(typeof myObj)
+console.log(typeof myFunction)
+myFunction()
+console.log(typeof bigNumber)
+console.log(typeof anotherId)
