887a8518a8b61e4f4f02e9c03344f6be42ed974d
Sandesh <163713139+Mark-085@users.noreply.github.com>
2025-01-26 03:44:28 +0000

    for loop , break , continue

--- 5_iterations/one.js
+++ 5_iterations/one.js
@@ -30,14 +30,14 @@ for (let index = 0; index < myArray.length; index++) {
 
 // break and continue
 
-for (let index = 1; index <= 20; index++) {
-    if (index == 5) {
-        console.log(`Detected 5`);
-        break
-    }
-   console.log(`Value of i is ${index}`);
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//    console.log(`Value of i is ${index}`);
     
-}
+// }
 
 for (let index = 1; index <= 20; index++) {
     if (index == 5) {
